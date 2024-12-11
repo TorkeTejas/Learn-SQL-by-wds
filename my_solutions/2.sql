9868a466a24d88b1224a8ecb91cc4e59c352a4d2
TorkeTejas <tejastorke528@gmail.com>
2024-12-11 06:24:42 +0530

    changed 2.sql and remove sql.2

--- my_solutions/2.sql
+++ my_solutions/2.sql
@@ -1,3 +1,3 @@
--- SELECT bands.name AS 'Band Name'
--- FROM bands;
-alter table bands rename column name to 'Band Name';
+SELECT bands.name AS 'Band Name'
+FROM bands;
+-- alter table bands rename column name to 'Band Name';
